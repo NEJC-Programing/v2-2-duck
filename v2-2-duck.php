5a0b3354e05a83612645e642faaec8646b0fad6f
NHTHEBEST <noreply.nhthebest@gmail.com>
2018-05-08 13:46:42 +1200

    inital

---
+++ v2-2-duck.php
@@ -0,0 +1,25 @@
+<?php
+define("v2_ver", 0);
+
+define("url", "http://noam.hadar.net.nz");
+
+define("jsonfile", url."/v2_database_".v2_ver.".json");
+
+define("jsondata", file_get_contents(jsonfile));
+
+$data = json_decode(jsondata);
+
+define("line_end",";");
+
+if (sizeof($argv)!=3){
+    die("use: [infile] [outfile]");
+}
+
+define("infile", $argv[1]);
+define("outfile", $argv[2]);
+
+$temp = $data->code;
+if ($temp->ver != v2_ver){
+    die("invaled config please change config url to a working one");
+}
+
