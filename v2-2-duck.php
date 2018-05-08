c879e91b2b6a146cf5a0530d08cf6bf5d6ae658e
NHTHEBEST <noreply.nhthebest@gmail.com>
2018-05-08 14:18:35 +1200

    start

--- v2-2-duck.php
+++ v2-2-duck.php
@@ -18,8 +18,23 @@ if (sizeof($argv)!=3){
 define("infile", $argv[1]);
 define("outfile", $argv[2]);
 
-$temp = $data->code;
-if ($temp->ver != v2_ver){
+$code = $data->code;
+if ($code->ver != v2_ver){
     die("invaled config please change config url to a working one");
 }
 
+$file = file_get_contents(infile);
+
+$data = explode(line_end,$file);
+
+$outfile = "REM made by NHTHEBEST's v2-2-duck\n";
+$outfile .= "REM This Is BETA\n\n";
+
+foreach($line as $data){
+
+}
+
+
+function replace($line){
+    $line = str_replace($find,$replace,$line);
+}
